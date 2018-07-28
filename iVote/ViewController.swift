66001b3c7bf46040e65b5a63f8ed9a23a02a259a
BFar <Bryan@skylabapps.com>
2018-07-27 22:40:30 -0400

    Initial Commit

---
+++ iVote/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iVote
+//
+//  Created by BFar on 7/27/18.
+//  Copyright © 2018 iVote. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
